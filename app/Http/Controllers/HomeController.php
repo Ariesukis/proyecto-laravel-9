9a1e1053452c8f464e84c50290df8421c6fe2d96
Aries Ramírez <aries.kivitoz@gmail.com>
2022-06-07 00:10:04 -0500

    se hizo mix son sass y se le dio estilo al home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Event;
 
 class HomeController extends Controller
 {
     public function index()
     {
         $categories = Category::all();
+        $events = Event::all();
         //dd ($categories);
 
         // eventos -> una categoria
         // categoria -> muchos eventos
 
-        return view('events', ['categories' => $categories]);
+        return view('events', [
+            'categories' => $categories,
+            'events' => $events
+        ]);
     }
 }
--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -1,22 +1,57 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eventos de tecnologia</title>
-</head>
-<body>
+@extends('layouts.base')
 
-    <h1>Eventos</h1>
-    
-    <ol>
-        @foreach($categories as $category)
-            <li>
-                {{ $category->name }}
-            </li>
-        @endforeach
-    </ol>    
+@section('banner')
+<div class="col-sm-12 col-md-10 banner-container text-center">
+    <img src="https://source.unsplash.com/1280x720/?Development Conferences" class="img-fluid banner-image" alt="Banner principal">
+    <div class="text-block">
+        <h4 class="text-block-title">YOUDEVS FEST</h4>
+        <p class="text-block-subtitle mb-2">EN MI DEPA</p>
+        <a href="" class="btn btn-sm btn-primary-yellow rounded-12">Ver Detalle</a>
+    </div>
+</div>
+@endsection
 
-</body>
-</html>
+@section('content')
+<main class="row justify-content-center">
+
+    <div class="col-10 text-center text-white pt-5 pb-2">
+        <h1>Eventos disponibles</h1>
+    </div>
+
+    <!-- Filtro -->
+    <div class="d-flex justify-content-center">
+        <div class="col-auto lead m-1">
+            <span id="passwordHelpInline" class="form-text text-white fw-light">
+                <i class="fas fa-filter"></i>
+                Filtrar por categoría
+            </span>
+        </div>
+        <div class="col-auto m-1">
+            <select class="form-select">
+                @foreach ($categories as $category)
+                    <option>{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+    </div>
+
+    <!-- Grid de eventos -->
+    <div class="col-md-10 col-12 py-3">
+        <div class="row justify-content-center events-grid">
+
+            @foreach($events as $event)
+            <div class="col-md-4 col-sm-12 text-center event mb-4">
+                <img src="{{$event->img_banner}}" class="img-fluid" alt="Norway" style="width:100%; height:100%; object-fit: cover;">
+                <div class="text-block">
+                    <h4 class="text-block-title">{{$event->title}}</h4>
+                    <p class="text-block-p mb-0 fw-400">2{{$event->date_event}}</p>
+                    <p class="text-block-p mb-0 fw-400">{{$event->place_event}}</p>
+                    <a href="" class="btn btn-sm btn-primary-yellow rounded-12">Ver Detalle</a>
+                </div>
+            </div>
+            @endforeach
+
+        </div>
+    </div>
+</main>
+@endsection
